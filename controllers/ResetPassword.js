39ebb4295c8bf12f46a0ecfea4eaed135bd91bc1
anshika-maurya <anshikamaurya6400@gmail.com>
2024-10-25 21:40:32 +0530

    initial

--- controllers/ResetPassword.js
+++ controllers/ResetPassword.js
@@ -26,7 +26,7 @@ exports.resetPasswordToken = async (req, res) => {
     console.log("DETAILS", updatedDetails)
 
     // const url = `http://localhost:3000/update-password/${token}`
-    const url = `https://studynotion-ivory.vercel.app/update-password/${token}`
+    const url = `https://ed-tech-platform-khaki.vercel.app/update-password/${token}`
 
     await mailSender(
       email,
