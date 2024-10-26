dd9cd4662f74f52de5718e24a753fc7d0f01c96a
anshika-maurya <anshikamaurya6400@gmail.com>
2024-10-27 01:35:39 +0530

    initial

--- controllers/Category.js
+++ controllers/Category.js
@@ -53,6 +53,9 @@ exports.categoryPageDetails = async (req, res) => {
         path: "courses",
         match: { status: "Published" },
         populate: "ratingAndReviews",
+        populate: {
+          path: "instructor",
+        },
       })
       .exec()
 
@@ -84,6 +87,9 @@ exports.categoryPageDetails = async (req, res) => {
       .populate({
         path: "courses",
         match: { status: "Published" },
+        populate: {
+          path: "instructor",
+        },
       })
       .exec()
     console.log()
