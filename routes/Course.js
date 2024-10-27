68aa54a1bd3f24f37991168997992efced6de6e4
anshika-maurya <anshikamaurya6400@gmail.com>
2024-10-27 13:37:19 +0530

    initial

--- routes/Course.js
+++ routes/Course.js
@@ -94,6 +94,7 @@ router.delete("/deleteCourse", deleteCourse)
 router.post("/createCategory", auth, isAdmin, createCategory)
 router.get("/showAllCategories", showAllCategories)
 router.post("/getCategoryPageDetails", categoryPageDetails)
+// router.post("/addCourseToCategory", auth, isInstructor, addCourseToCategory)
 
 // ********************************************************************************************************
 //                                      Rating and Review
