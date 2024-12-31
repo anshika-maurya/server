4d9a203f52941d0779fa46b02a25320293209a61
anshika-maurya <anshikamaurya6400@gmail.com>
2024-12-31 11:11:29 +0530

    fix cors origin

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"*",
+		origin:["https://ed-tech-platform-khaki.vercel.app/","http://localhost:3000"],
 		credentials:true,
 	})
 )
